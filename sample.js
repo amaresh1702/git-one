e35fd5d460a9faba54b0ff280e9d494154262b17
amaresh <amareshpanakala123@gmail.com>
2020-12-07 12:19:49 +0530

    first commit

---
+++ sample.js
@@ -0,0 +1,6 @@
+var process=function(input,index,length){
+  //   var output=input.substr(index,length);
+     var outputArray=input.substr(index,length).split("");
+     return outputArray;
+ }
+ console.log(process("amaresh",3,3));
